24d74f95c2db12d81528d4fedb90e061abd9be97
ramoni13 <46489729+ramoni13@users.noreply.github.com>
2026-09-17 17:52:23 +0200

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -2,9 +2,6 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      // ⚠️ react-native-reanimated DOIT être le dernier plugin
-      'react-native-reanimated/plugin',
-    ],
+    // react-native-reanimated 4.x n'a plus besoin du plugin Babel
   };
 };
